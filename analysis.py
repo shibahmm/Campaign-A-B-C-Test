66a34a9061b0f13ae36ecd48794226c37ecb7eb9
Khang Le <le000326@umn.edu>
2025-03-05 18:44:17 -0600

    add analysis.py

---
+++ analysis.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("E:/Side Projects/Marketing-Campaign-A-B-Test/WA_Marketing-Campaign.csv")
+print(df)
--- test.py
+++
@@ -1,6 +0,0 @@
-def test(a):
-    a = 2
-    a = a +1 
-    print(a)
-
-test(3)
